903aff06fd797a6171f4f38733a9a5e23b38abac
Faishal Mukhammad Syarief <faishalmukhammadsyarief@gmail.com>
2023-11-03 14:41:25 +0700

    Marquee Title

--- app/src/main/java/com/zhalz/friendzy/MainActivity.kt
+++ app/src/main/java/com/zhalz/friendzy/MainActivity.kt
@@ -5,7 +5,6 @@ import android.graphics.Shader
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.databinding.DataBindingUtil
 import com.zhalz.friendzy.databinding.ActivityMainBinding
 
@@ -20,5 +19,8 @@ class MainActivity : AppCompatActivity() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             binding.iv.setRenderEffect(RenderEffect.createBlurEffect(10F,10F,Shader.TileMode.MIRROR))
         }
+
+        binding.title.isSelected = true
+
     }
 }
