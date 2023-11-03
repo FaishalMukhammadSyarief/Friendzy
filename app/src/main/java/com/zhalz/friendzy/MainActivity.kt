0dc4a485de874d1382827d309c21bf16c29a6de4
Faishal Mukhammad Syarief <faishalmukhammadsyarief@gmail.com>
2023-11-03 14:19:47 +0700

    Initial commit

---
+++ app/src/main/java/com/zhalz/friendzy/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.zhalz.friendzy
+
+import android.graphics.RenderEffect
+import android.graphics.Shader
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.databinding.DataBindingUtil
+import com.zhalz.friendzy.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy { DataBindingUtil.setContentView(this, R.layout.activity_main) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            binding.iv.setRenderEffect(RenderEffect.createBlurEffect(10F,10F,Shader.TileMode.MIRROR))
+        }
+    }
+}
