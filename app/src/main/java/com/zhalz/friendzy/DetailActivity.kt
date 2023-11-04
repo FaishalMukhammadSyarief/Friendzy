967c25177cc0fb00c88c305f8b6fdef49de660ee
Faishal Mukhammad Syarief <faishalmukhammadsyarief@gmail.com>
2023-11-04 10:04:28 +0700

    Migrate the design to activity_detail.xml

--- app/src/main/java/com/zhalz/friendzy/DetailActivity.kt
+++ app/src/main/java/com/zhalz/friendzy/DetailActivity.kt
@@ -2,10 +2,19 @@ package com.zhalz.friendzy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.zhalz.friendzy.databinding.ActivityMainBinding
 
 class DetailActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        DataBindingUtil.setContentView(this, R.layout.activity_detail)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+
+        binding.title.isSelected = true
     }
 }
--- app/src/main/java/com/zhalz/friendzy/MainActivity.kt
+++ app/src/main/java/com/zhalz/friendzy/MainActivity.kt
@@ -1,8 +1,5 @@
 package com.zhalz.friendzy
 
-import android.graphics.RenderEffect
-import android.graphics.Shader
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
@@ -15,12 +12,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-//            binding.iv.setRenderEffect(RenderEffect.createBlurEffect(10F,10F,Shader.TileMode.MIRROR))
-//        }
-
-        binding.title.isSelected = true
 
     }
 }
