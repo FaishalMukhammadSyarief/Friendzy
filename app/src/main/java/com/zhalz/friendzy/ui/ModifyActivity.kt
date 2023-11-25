d634fca20edc920faa1a0d091ec1691c0c5f694c
Faishal Mukhammad Syarief <faishalmukhammadsyarief@gmail.com>
2023-11-25 19:03:54 +0700

    Open Camera

--- app/src/main/java/com/zhalz/friendzy/ui/ModifyActivity.kt
+++ app/src/main/java/com/zhalz/friendzy/ui/ModifyActivity.kt
@@ -1,28 +1,50 @@
 package com.zhalz.friendzy.ui
 
 import android.app.DatePickerDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
 import android.icu.util.Calendar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.lifecycleScope
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.zhalz.friendzy.R
 import com.zhalz.friendzy.data.database.AppDatabase
 import com.zhalz.friendzy.data.database.FriendDao
 import com.zhalz.friendzy.data.database.FriendEntity
 import com.zhalz.friendzy.databinding.ActivityModifyBinding
 import kotlinx.coroutines.launch
+import java.io.File
 
 class ModifyActivity : AppCompatActivity() {
 
     private val binding: ActivityModifyBinding by lazy { DataBindingUtil.setContentView(this, R.layout.activity_modify) }
     private val friendManager: FriendDao by lazy { AppDatabase.getInstance(this).friendDao() }
 
+    private lateinit var photoFile: File
+
     var name = ""
     var birth = ""
     var description = ""
     private var idFriend = 0
+
+    private var cameraLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == RESULT_OK) {
+                val takenImage = BitmapFactory.decodeFile(photoFile.absolutePath)
+                binding.ivProfile.setImageBitmap(takenImage)
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +57,8 @@ class ModifyActivity : AppCompatActivity() {
         idFriend = intent.getIntExtra("id", 0)
 
         toolbarConfiguration()
+
+        photoFile = createImageFile()
 
     }
 
@@ -154,6 +178,70 @@ class ModifyActivity : AppCompatActivity() {
 
     fun finishActivity() {
         finish()
+    }
+
+    fun addPhoto(){
+
+        val items = arrayOf("Camera", "Cancel")
+
+        MaterialAlertDialogBuilder(this)
+            .setTitle("Select From")
+            .setItems(items) { _, which ->
+                when (which) {
+                    0 -> checkPermissionCamera()
+                    1 -> Unit
+                }
+            }
+            .show()
+    }
+
+    private fun createImageFile(): File {
+        val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile("PHOTO_", ".jpg", storageDir)
+    }
+
+    private fun openCamera() {
+        val photoFileUri =
+            FileProvider.getUriForFile(this, "com.zhalz.friendzy.file-provider", photoFile)
+
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE).apply {
+            putExtra(MediaStore.EXTRA_OUTPUT, photoFileUri)
+        }
+        cameraLauncher.launch(cameraIntent)
+    }
+
+
+    /* -- RUNTIME PERMISSION -- */
+
+    private fun checkPermissionCamera() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            /** Request Permission **/
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA), REQUEST_CODE_CAMERA)
+        } else {
+            openCamera()
+        }
+    }
+
+    //Menentukan apa yang terjadi saat Permission diberikan/tidak diberikan
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == REQUEST_CODE_CAMERA) {
+            if (grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+                openCamera()
+            } else {
+                Toast.makeText(this, "Permission not Granted", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    companion object {
+        const val REQUEST_CODE_CAMERA = 100
     }
 
 }
