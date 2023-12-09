6e6cd3532c49219e4bfdfc0f47f961cf27a9c759
Faishal Mukhammad Syarief <faishalmukhammadsyarief@gmail.com>
2023-12-09 11:37:40 +0700

    adjust gradle for crocodic core

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
     id("kotlin-kapt")
     id("com.google.dagger.hilt.android")
     id("com.google.gms.google-services")
+    id("com.google.firebase.crashlytics")
+    id("com.google.firebase.firebase-perf")
 }
 
 android {
@@ -46,10 +48,8 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.github.crocodic-studio:AndroidCoreProject:4.0.11")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,6 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.google.dagger.hilt.android") version "2.48" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
+    id("com.google.firebase.crashlytics") version "2.9.2" apply false
+    id("com.google.firebase.firebase-perf") version "1.4.2" apply false
 }
