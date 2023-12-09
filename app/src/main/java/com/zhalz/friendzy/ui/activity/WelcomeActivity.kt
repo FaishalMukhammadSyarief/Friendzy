ada0ad44d145dcf65ddcfcf36031b6084a7765cb
Faishal Mukhammad Syarief <faishalmukhammadsyarief@gmail.com>
2023-12-09 11:56:23 +0700

    fix some warning

--- app/src/main/java/com/zhalz/friendzy/ui/activity/WelcomeActivity.kt
+++ app/src/main/java/com/zhalz/friendzy/ui/activity/WelcomeActivity.kt
@@ -1,6 +1,5 @@
 package com.zhalz.friendzy.ui.activity
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.view.WindowCompat
