20075745cd81af8f9b2790c411c82ab1a18ed480
Faishal Mukhammad Syarief <faishalmukhammadsyarief@gmail.com>
2023-12-10 23:22:47 +0700

    implement Core on FriendDao.kt

--- app/src/main/java/com/zhalz/friendzy/data/friend/FriendDao.kt
+++ app/src/main/java/com/zhalz/friendzy/data/friend/FriendDao.kt
@@ -1,22 +1,12 @@
 package com.zhalz.friendzy.data.friend
 
 import androidx.room.Dao
-import androidx.room.Delete
-import androidx.room.Insert
 import androidx.room.Query
-import androidx.room.Update
+import com.crocodic.core.data.CoreDao
 import kotlinx.coroutines.flow.Flow
 
 @Dao
-interface FriendDao {
-    @Insert
-    suspend fun insert(friend: FriendEntity)
-
-    @Update
-    suspend fun update(friend: FriendEntity)
-
-    @Delete
-    suspend fun delete(friend: FriendEntity)
+interface FriendDao: CoreDao<FriendEntity> {
 
     @Query("SELECT * FROM FriendEntity")
     fun getAll(): Flow<List<FriendEntity>>
