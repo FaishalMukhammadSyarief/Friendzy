222719b153192d869e66a58c1067977d2d3ec2d5
Faishal Mukhammad Syarief <faishalmukhammadsyarief@gmail.com>
2024-03-05 15:49:39 +0700

    [CREATE] Profile with id

--- app/src/main/java/com/zhalz/friendzy/data/user/UserEntity.kt
+++ app/src/main/java/com/zhalz/friendzy/data/user/UserEntity.kt
@@ -7,6 +7,7 @@ import androidx.room.PrimaryKey
 data class UserEntity(
     @PrimaryKey(autoGenerate = true)
     var databaseId: Int = 0,
+    val id: Int?,
     val name: String?,
     val school: String?,
     val description: String?
