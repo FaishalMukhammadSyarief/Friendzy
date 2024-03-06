8a5a80ca0e3ca963d9d93d49c8080dbd2436eede
Faishal Mukhammad Syarief <faishalmukhammadsyarief@gmail.com>
2024-03-06 15:23:46 +0700

    [CREATE] Search Feature

--- app/src/main/java/com/zhalz/friendzy/ui/search/SearchFragment.kt
+++ app/src/main/java/com/zhalz/friendzy/ui/search/SearchFragment.kt
@@ -9,21 +9,18 @@ import android.view.ViewGroup
 import androidx.appcompat.widget.SearchView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
 import com.crocodic.core.base.adapter.ReactiveListAdapter
 import com.zhalz.friendzy.R
-import com.zhalz.friendzy.data.friend.FriendEntity
+import com.zhalz.friendzy.data.user.UserEntity
 import com.zhalz.friendzy.databinding.FragmentSearchBinding
 import com.zhalz.friendzy.databinding.ItemFriendsBinding
 import com.zhalz.friendzy.ui.detail.DetailActivity
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class SearchFragment : Fragment() {
 
     private val viewModel: SearchViewModel by viewModels()
-
     private lateinit var binding: FragmentSearchBinding
 
     override fun onCreateView(
@@ -31,14 +28,12 @@ class SearchFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_search, container, false)
-
         binding.fragment = this
 
         binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
                 return false
             }
-
             override fun onQueryTextChange(query: String?): Boolean {
                 searchFriend(query)
                 return true
@@ -50,26 +45,21 @@ class SearchFragment : Fragment() {
 
     private fun searchFriend(query: String?) {
         val adapter =
-            ReactiveListAdapter<ItemFriendsBinding, FriendEntity>(R.layout.item_friends).initItem { _, data ->
+            ReactiveListAdapter<ItemFriendsBinding, UserEntity>(R.layout.item_friends).initItem { _, data ->
                 toDetail(data)
             }
 
-        lifecycleScope.launch {
-            viewModel.searchFriend(query).collect {
-                adapter.submitList(it)
-            }
-        }
-
+        viewModel.getListFriend(query)
+        viewModel.listFiltered.observe(viewLifecycleOwner) { adapter.submitList(it) }
         binding.rvQueryFriend.adapter = adapter
     }
 
-    private fun toDetail(data: FriendEntity) {
+    private fun toDetail(data: UserEntity) {
         val toDetail = Intent(requireContext(), DetailActivity::class.java).apply {
-            putExtra("name", data.name)
-            putExtra("birth", data.birth)
-            putExtra("description", data.description)
-            putExtra("photo", data.photo)
             putExtra("id", data.id)
+            putExtra("name", data.name)
+            putExtra("school", data.school)
+            putExtra("description", data.description)
         }
         startActivity(toDetail)
     }
--- app/src/main/java/com/zhalz/friendzy/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/zhalz/friendzy/ui/search/SearchViewModel.kt
@@ -1,10 +1,34 @@
 package com.zhalz.friendzy.ui.search
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.crocodic.core.api.ApiObserver
 import com.zhalz.friendzy.base.BaseViewModel
+import com.zhalz.friendzy.data.response.ListFriendResponse
+import com.zhalz.friendzy.data.user.UserEntity
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class SearchViewModel @Inject constructor() : BaseViewModel() {
-    fun searchFriend(query: String?) = friendRepositoryImpl.searchFriend(query)
+
+    private val _loginResponse = MutableSharedFlow<ListFriendResponse>()
+    val loginResponse = _loginResponse.asSharedFlow()
+
+    val listFiltered = MutableLiveData<List<UserEntity>?>()
+
+    fun getListFriend(query: String?) = viewModelScope.launch {
+        ApiObserver.run({ apiService.getListFriend() }, false,
+            object : ApiObserver.ResponseListenerFlow<ListFriendResponse>(_loginResponse) {
+                override suspend fun onSuccess(response: ListFriendResponse) {
+                    super.onSuccess(response)
+                    listFiltered.postValue(response.data?.filter {
+                        it.name?.contains(query ?: "", true) ?: true
+                    })
+                }
+            } )
+    }
 }
