47e5754b0d72fb6a726609f70f595cb2054f2156
Faishal Mukhammad Syarief <faishalmukhammadsyarief@gmail.com>
2024-03-09 21:56:51 +0700

    [CLEAN] Unused import & a var

--- app/src/main/java/com/zhalz/friendzy/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/zhalz/friendzy/ui/profile/ProfileFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.core.content.res.ResourcesCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -27,8 +26,6 @@ class ProfileFragment : Fragment() {
     var name = ""
     var school = ""
     var desc = ""
-
-    var edt = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
